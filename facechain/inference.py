c69ec0ce26d385bd3616a70405937111fe47e2f6
Huang Tiancheng <athinkingneal@gmail.com>
2023-09-25 09:26:12 +0800

    label filtering finished

--- facechain/inference.py
+++ facechain/inference.py
@@ -490,6 +490,8 @@ def face_swap_fn(use_face_swap, gen_results, template_face):
     if use_face_swap:
         ## TODO
         out_img_list = []
+
+        # change with roop
         image_face_fusion = pipeline('face_fusion_torch',
                                      model='damo/cv_unet_face_fusion_torch', model_revision='v1.0.3')
         for img in gen_results:
